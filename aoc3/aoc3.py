58033b5f13e01b2b35ba1c74f3696a7cd159e8c5
Daniel Hussey <daniel@hussey.io>
2024-12-03 17:30:20 +1100

    Updates

--- aoc3/aoc3.py
+++ aoc3/aoc3.py
@@ -8,11 +8,8 @@ while rIdx != -1:
     lIdx = memory.find("mul(", lIdx)
     rIdx = memory.find(")", lIdx)
     if rIdx != -1:
-        substr = memory[lIdx+4:rIdx]
-        print(substr)
-        if len(substr.split(",")) == 2 and all(map(str.isnumeric, substr.split(","))):
-            sum += int(substr.split(",")[0])*int(substr.split(",")[1])
+        substrs = memory[lIdx+4:rIdx].split(",")
+        if len(substrs)==2 and all(map(str.isnumeric, substrs)):
+            sum += int(substrs[0]) * int(substrs[1])            
         lIdx+=4
-
-
 print(sum)
