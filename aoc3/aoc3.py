2b6216cbd5ab80d32b3598adddba5c5f4a90389e
Daniel Hussey <daniel@hussey.io>
2024-12-03 18:05:23 +1100

    Updates

--- aoc3/aoc3.py
+++ aoc3/aoc3.py
@@ -1,12 +1,13 @@
-memory = "".join([line.strip() for line in open("input.txt", "r")])
-
-pos = 0
-substrs = []
-sum=0
+memory = "".join([line.strip("\n") for line in open("input.txt", "r")])
+pos, sumPart1, sumPart2 = 0
 while (pos := memory.find("mul(", pos)) != -1:
+    mul_enabled = memory.rfind("do()", 0, pos) >= memory.rfind("don't()", 0, pos)
     if (rIdx := memory.find(")", pos)) != -1:
         substrs = memory[pos+4:rIdx].split(",")
         if len(substrs)==2 and all(map(str.isnumeric, substrs)):
-            sum += int(substrs[0]) * int(substrs[1])            
-        pos+=4
-print(sum)
+            sumPart1 += int(substrs[0]) * int(substrs[1])
+            if mul_enabled:
+                sumPart2 += int(substrs[0]) * int(substrs[1])
+        pos+=1
+        
+print(sumPart1, sumPart2)
