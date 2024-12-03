363b1464723c5e7b83dd0443fe4cd078e9b4241e
Daniel Hussey <daniel@hussey.io>
2024-12-03 17:33:51 +1100

    Updates

--- aoc3/aoc3.py
+++ aoc3/aoc3.py
@@ -1,15 +1,12 @@
 memory = "".join([line.strip() for line in open("input.txt", "r")])
 
-lIdx = 0
-rIdx = 0
+pos = 0
 substrs = []
 sum=0
-while rIdx != -1:
-    lIdx = memory.find("mul(", lIdx)
-    rIdx = memory.find(")", lIdx)
-    if rIdx != -1:
-        substrs = memory[lIdx+4:rIdx].split(",")
+while (pos := memory.find("mul(", pos)) != -1:
+    if (rIdx := memory.find(")", pos)) != -1:
+        substrs = memory[pos+4:rIdx].split(",")
         if len(substrs)==2 and all(map(str.isnumeric, substrs)):
             sum += int(substrs[0]) * int(substrs[1])            
-        lIdx+=4
+        pos+=4
 print(sum)
