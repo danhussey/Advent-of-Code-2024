a40acb228dcba9a564ff21eeae790ae02403eaf2
Daniel Hussey <daniel@hussey.io>
2024-12-20 14:39:02 +1100

    Updates

--- aoc7/aoc7.py
+++ aoc7/aoc7.py
@@ -2,24 +2,14 @@
 with open("aoc7/input.txt", "r") as file:
     lines = [line.strip().split(" ") for line in file]
 
-# Func that gets all possible equation results 
+# Func that gets all possible equation results and returns true if test result can be reached
 from itertools import product
 from functools import reduce
-
-def apply_operator(x, y, operator):
-    if operator=="+":
-        return x+y
-    elif operator=="*":
-        return x*y
-
-
 def is_possibly_true_equation (test_value: int, operands: list[int]):
     possible_operations_lists = list(product(["*", "+"], repeat=len(operands)-1))
-
     
     for operations_list in possible_operations_lists:
         vals_ops = zip(operands[1:], operations_list)
-
         res = reduce(lambda acc, val_op: (acc + val_op[0] if val_op[1]=="+" else acc*val_op[0]),
                      vals_ops,
                      operands[0])
@@ -29,8 +19,6 @@ def is_possibly_true_equation (test_value: int, operands: list[int]):
             return True
     return False
 
-
-print(is_possibly_true_equation(3267, [81, 40, 27]))
 # Collects test values with valid equation results
 sum = 0
 for line in lines:
@@ -39,3 +27,21 @@ for line in lines:
     if is_possibly_true_equation(test_val, operands):
         sum += test_val
 print(sum)
+
+
+# Part 2
+def is_possibly_true_equation_pt2 (test_value: int, operands: list[int]):
+    possible_operations_lists = product(["*", "+", "||"], repeat=len(operands)-1)
+    
+    for operations_list in possible_operations_lists:
+        res = operands[0]
+        for vals_ops in zip(operands[1:], operations_list):
+            
+        res = reduce(lambda acc, val_op: (acc + val_op[0] if val_op[1]=="+" else acc*val_op[0]),
+                     vals_ops,
+                     operands[0])
+
+        if res == test_value:
+            print("Found " + str(operations_list) + " for " + str(operands) + " == " + str(test_value))
+            return True
+    return False
